1fe1a5a7a08561618e3e4258c5f043dc847e24bc
Andrey Gomes <andreygdsn@gmail.com>
2026-01-26 21:00:59 -0300

    fix: exportacao hash and add todo.md

--- src/br/ifba/edu/inf011/strategy/ExportacaoStrategy.java
+++ src/br/ifba/edu/inf011/strategy/ExportacaoStrategy.java
@@ -3,14 +3,15 @@ package br.ifba.edu.inf011.strategy;
 import br.ifba.edu.inf011.model.documentos.Documento;
 import br.ifba.edu.inf011.model.documentos.Privacidade;
 
+import java.time.LocalDateTime;
+
 public class ExportacaoStrategy implements AutenticadorStrategy{
   @Override
   public String gerarNumero(Documento documento){
-    if (documento.getPrivacidade() == Privacidade.SIGILOSO) { 
-      String sufixo = (documento.getNumero() != null) ? String.valueOf(documento.getNumero().hashCode()) : String.valueOf(documento.hashCode());
-      return "SECURE-" + sufixo;
+    if (documento.getPrivacidade() == Privacidade.SIGILOSO) {
+      return "SECURE-" + documento.hashCode() + LocalDateTime.now().hashCode();
     } else {
-      return "PUB-" + documento.hashCode();
+      return "PUB-" + documento.hashCode() + LocalDateTime.now().hashCode();
     }
   }
 }
